73e52235dd4f658a295df5c006adbc500c822f59
Nafi Baliya <baliya363@gmail.com>
2021-01-08 23:36:26 +0700

    add fetch and store functionality

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 // global import
 import "./src/toggleSidebar.js";
 import "./src/cart/toggleCart.js";
+
+import fetchProducts from "./src/fetchProducts.js";
+import { setupStore, store } from "./src/store.js";
+
+const init = async () => {
+  const products = await fetchProducts().catch((err) => {
+    console.log(err);
+  });
+  setupStore(products);
+  console.log(store);
+};
+
+window.addEventListener("DOMContentLoaded", init);
